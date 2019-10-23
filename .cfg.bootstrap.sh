313701dfd0ef458187ac22b7ebf7707e829387f9
Matt Gautreau <gautreau@amazon.com>
2019-10-23 16:40:22 -0700

    add bootstrap script

---
+++ .cfg.bootstrap.sh
@@ -0,0 +1,8 @@
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+sh $HOME/.config/nvim/bootstrap.sh
+
+# git config
+git config --global pull.rebase true
+git config --global core.excludesfile ~/.gitignore_global
